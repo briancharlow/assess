48edbb27abdcdf3757185067f9d64b265b72219e
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-05-12 11:16:59 +0300

    adds angles code

--- angle.js
+++ angle.js
@@ -1,6 +1,26 @@
-Write a JavaScript program to find the types of a given angle.
+// Write a JavaScript program to find the types of a given angle.
 
-Acute angle: An angle between 0 and 90 degrees.
-Right angle: An 90 degree angle.
-Obtuse angle: An angle between 90 and 180 degrees.
-Straight angle: A 180 degree angle.
+// Acute angle: An angle between 0 and 90 degrees.
+// Right angle: An 90 degree angle.
+// Obtuse angle: An angle between 90 and 180 degrees.
+// Straight angle: A 180 degree angle.
+
+function angle(){
+    if(random===90){
+    console.log("this is a right angle");
+    }
+    else if(random<90){
+        console.log("this is an acute angle")
+    }
+     else if(random>90 && random<180){
+        console.log("this is an obtuse angle")
+    }
+     else if(random===180){
+        console.log("this is a straight angle")
+    }
+    else{
+        console.log("this is a reflex angle")
+    }
+}
+let random=prompt("please enter the degrees");
+angle();
--- expression.js
+++ expression.js
@@ -1,6 +1,11 @@
-// .hat will be logged to the console from the below expression. explain why.
- W
+// what will be logged to the console from the below expression. explain why.
+ 
 
 let sum = 5+ 5+"5";
 
 console.log(sum);
+
+//answer
+
+// the output will be 105 since the computer adds the firts 5s together to get 10
+// then picks up the third 5 as a string and concatentaes it to the result of the first addition
