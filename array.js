872ac0c47f46212697aa0012395cf78011274639
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-05-12 11:24:03 +0300

    adds the array code

--- array.js
+++ array.js
@@ -1,2 +1,16 @@
 // Write a JavaScript program to create an array by taking the
 //  first and last elements from a given array of integers. The length must be larger than or equal to 1.
+
+function createArrayFromFirstAndLastElements(arr) {
+  if (arr.length < 1) {
+    throw new Error("Array length must be larger than or equal to 1");
+  }
+
+  var firstElement = arr[0];
+  var lastElement = arr[arr.length - 1];
+
+  return [firstElement, lastElement];
+}
+var integers = [1, 2, 3, 4, 5];
+var result = createArrayFromFirstAndLastElements(integers);
+console.log(result);
